4d8c603a1c6792e6ef9b84be517ddbc1a3308329
hwanynaver <hwanys2@naver.com>
2021-10-04 23:05:01 +0900

    hwanynaver  2021-10-04

--- github_push.py
+++ github_push.py
@@ -7,12 +7,12 @@ os.chdir(path)
 user_name = os.popen('git config --list ').read().split('user.name=')[1].split('\n')[0]
 
 print(os.path)
-output = os.popen('git init').read()
+output = os.popen('git init')
 print(output)
 output = os.popen('git add .').read()
 print(output)
-output = os.popen("git commit -m '{}  {}'".format(user_name, date.today())).read()
+output = os.popen('git commit -m "{}  {}"'.format(user_name, date.today())).read()
 print(output)
 output = os.popen('git push origin main').read()
 print(output)
-##
+###
