ad714b99eb07d4e643cde126147f541beab282bd
hwanys2 <hwanys2na@gmail.com>
2021-10-05 11:13:13 +0900

    hwanys2  2021-10-05

--- github_push.py
+++ github_push.py
@@ -4,17 +4,14 @@ from datetime import date
 path = os.path.realpath(__file__).split('github')[0]
 os.chdir(path)
 
-# user_name = os.popen('git config --list ').read().split('user.name=')[1].split('\n')[0]
-
+user_name = os.popen('git config --list ').read().split('user.name=')[1].split('\n')[0]
 
 print(os.path)
-output = os.popen('git init').read()
+output = os.popen('git init')
 print(output)
 output = os.popen('git add .').read()
 print(output)
-output = os.popen("git commit -m asa").read()
+output = os.popen('git commit -m "{}  {}"'.format(user_name, date.today())).read()
 print(output)
 output = os.popen('git push origin main').read()
 print(output)
-
-# this is good
