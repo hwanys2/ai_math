afbe5a1ef0512c71a33b2c354f1a1d6431654f54
anjongbin <sunnyfenix@hanmail.net>
2021-10-05 11:02:58 +0900

    asa

--- github_push.py
+++ github_push.py
@@ -4,14 +4,15 @@ from datetime import date
 path = os.path.realpath(__file__).split('github')[0]
 os.chdir(path)
 
-user_name = os.popen('git config --list ').read().split('user.name=')[1].split('\n')[0]
+# user_name = os.popen('git config --list ').read().split('user.name=')[1].split('\n')[0]
+
 
 print(os.path)
 output = os.popen('git init').read()
 print(output)
 output = os.popen('git add .').read()
 print(output)
-output = os.popen("git commit -m '{}  {}'".format(user_name, date.today())).read()
+output = os.popen("git commit -m asa").read()
 print(output)
 output = os.popen('git push origin main').read()
 print(output)
