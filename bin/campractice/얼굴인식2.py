f5e99ae79ab5c5c7e2be54e785a32536a32c5c14
min <minjk13@gmail.com>
2021-10-24 21:43:58 +0900

    min  2021-10-24

--- bin/campractice/얼굴인식2.py
+++ bin/campractice/얼굴인식2.py
@@ -5,6 +5,7 @@ cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video width
 cam.set(4, 480) # set video height
 face_detector = cv2.CascadeClassifier('bin/campractice/haarcascades/haarcascade_frontalface_default.xml')
+#face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
 # For each person, enter one numeric face id
 face_id = input('\n enter user id end press <return> ==>  ')
@@ -18,7 +19,7 @@ while(True):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
         count += 1
         # Save the captured image into the datasets folder
         cv2.imwrite("bin/campractice/dateset/" + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
---
+++ min/2_teachable/con_os.py
@@ -0,0 +1,5 @@
+import os
+import subprocess
+#subprocess.call(['C:\\Temp\\a b c\\Notepad.exe', 'C:\\test.txt'])
+#subprocess.call('C:\\Temp\\dir')
+os.system("D:\\Project\\py\\VS\\Code.exe")
